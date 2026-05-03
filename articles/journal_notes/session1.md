f59258c97418def5f16a966a7b3be9094bec0931
Bianca Eni <b.b.eni@student.tue.nl>
2026-05-03 13:42:50 +0200

    Fix add span script to ignore old wrappings and scan all files automatically

--- articles/journal_notes/session1.md
+++ articles/journal_notes/session1.md
@@ -4,6 +4,6 @@ title: The Beginning
 ---
 ### The Beginning - <span class='name-sillidious'> Sillidious </span> Joins the Party
 #### March 19, 2026
-<span class='name-thalia'> Thalia </span>, <span class='name-kaimi'> Kaimi </span>, <span class='name-talon'> Talon </span>, <span class='name-lachlan'> Lachlan </span>
+<span class='name-thalia'> Thalia </span>, <span class='name-sillidious'> Sillidious </span>, <span class='name-kaimi'> Kaimi </span>, <span class='name-talon'> Talon </span>, <span class='name-lachlan'> Lachlan </span>
 
-<span class='name-sillidious'> Sillidious </span> appears
+<span class='name-kaimi'> Kaimi </span>
--- process_journal_input/add_spans.py
+++ process_journal_input/add_spans.py
@@ -1,24 +1,63 @@
-import argparse
+# import argparse
+import re
+import os
 
-# Add parser for command line argument
-parser = argparse.ArgumentParser(description="Select which markdown file to parse")
-parser.add_argument('-n', required=True, help="Choose session number")
-arg = parser.parse_args()
-session_number = arg.n
+# no longer using cli arguments 
 
-# set file to modify
-filename= f'../articles/journal_notes/session{session_number}.md'
-with open(filename, 'r') as f:
-    text = f.read()
+# # Add parser for command line argument
+# parser = argparse.ArgumentParser(description="Select which markdown file to parse")
+# parser.add_argument('-n', required=True, help="Choose session number")
+# arg = parser.parse_args()
+# session_number = arg.n
 
+# array of pre-defined chacacters
 characters = ['Thalia', 'Talon', 'Kaimi', 'Lachlan', 'Sillidious']
-# check if it needs formatting else don't do it
-if not text.__contains__('span'):
+# folder of the journal files to modify
+folder = '../articles/journal_notes/'
+
+# Replace all unwrapped names
+def replace_unwrapped_names(text, name):
+    wrapped = f"<span class='name-{name.lower()}'> {name} </span>"
+    # replace already wrapped with a placeholder so it doesn't interfere with subsequent replacements
+    placeholder = f"WRAPPED_{name.upper()}"
+    text = text.replace(wrapped, placeholder)
+    
+    # replace all remaining unwrapped instances
+    text = re.sub(
+        rf'\b{name}\b',
+        wrapped,
+        text,
+        flags=re.IGNORECASE
+    )
+    # restore wrapped version
+    text = text.replace(placeholder, wrapped)
+    
+    return text
+
+# Go through a file, format it and save the results
+def format_file(filename):
+    # read file
+    filename_path = folder+filename
+    with open(filename_path, 'r') as f:
+        text = f.read()
+    # wrap
     for character in characters:
-        text = text.replace(character, f'<span class=\'name-{character.lower()}\'> {character} </span>')
-        
-    print(text)
-    with open(filename, 'w') as f:
+        text = replace_unwrapped_names(text, character)
+    # write to the file
+    with open(filename_path, 'w') as f:
         f.write(text)
-else:
-    print("This file has been formatted already")
+        
+# Go through all journal files available in the folder and format
+for file in os.listdir(folder):
+    filename = os.fsdecode(file)
+    if filename.endswith(".md"):
+        print(f"Formatting file {filename}: ")
+        format_file(filename)
+
+
+
+
+# Write back to file
+
+
+print("Formatting complete!")
